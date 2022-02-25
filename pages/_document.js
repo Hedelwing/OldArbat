a45a56d1cd9463736e0cb84c3c9e99d0e90c07fd
Christine Pnalina <35448631+Hedelwing@users.noreply.github.com>
2022-02-25 18:42:24 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -42,24 +42,6 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <meta name="yandex-verification" content="f7c5790fa8fe9e77" />
                     <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&display=swap" rel="stylesheet" type="text/css" />
-                    {process.env.NODE_ENV === 'production' &&
-                        <script
-                            type="text/javascript"
-                            dangerouslySetInnerHTML={{
-                                __html: `
-                                (function(m,e,t,r,i,k,a){m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-                                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                             
-                                ym(72061618, "init", {
-                                    clickmap:true,
-                                    trackLinks:true,
-                                    accurateTrackBounce:true
-                                });
-                                `
-                            }}
-                        />
-                    }
                 </Head>
                 <body>
                     <Main />
